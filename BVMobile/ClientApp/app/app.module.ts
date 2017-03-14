e7a2dbb2d801c40942929e8fe0443f10ed0962f2
iomek <iomekam3@gmail.com>
2017-03-13 23:37:10 -0700

    Adding Material. Taking away prerender

--- BVMobile/ClientApp/app/app.module.ts
+++ BVMobile/ClientApp/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdIconRegistry } from '@angular2-material/icon';
+
 import { AppComponent } from './app.component'
 import { DeviceMockupModule } from '../deviceMockup/device-mockup.module'
 
@@ -12,9 +17,14 @@ import { DeviceMockupModule } from '../deviceMockup/device-mockup.module'
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        MdCardModule,
+        MdButtonModule,
+        MdIconModule,
         DeviceMockupModule
     ],
     providers: [
+        
+        MdIconRegistry
     ]
 })
 export class AppModule { }
