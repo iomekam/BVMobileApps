f65e4f62085fdf56dc53f2c7e8ac3eb100ea645a
iomek <iomekam3@gmail.com>
2017-03-19 23:48:08 -0700

    checking GET config

--- server.js
+++ server.js
@@ -19,11 +19,3 @@ const forceSSL = function() {
 // to use the forceSSL
 // middleware
 app.use(forceSSL());
-
-const path = require('path');
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
